cdf57f002bea630eb3e0d9fc50a1cc66c3a1f53f
nekitvand <nekitvand@gmail.com>
2019-07-26 19:50:24 +0300

    Update url from test

--- App/core.py
+++ App/core.py
@@ -1,22 +1,20 @@
 import requests
 from App.response import AssertableResponse
-import pytest
-import os
+
 
 class ApiCore(object):
     def __init__(self):
         self.api_key = "285a9f81"
-        self._base_url = os.environ["BASE_URL"]
 
-    def get(self,query):
-        return requests.get(url=f'{self._base_url}?apikey={self.api_key}', params=query)
+    def get(self,query,url):
+        return requests.get(url=f'{url}?apikey={self.api_key}', params=query)
 
 class CinemaApi(ApiCore):
 
     def __init__(self):
         super().__init__()
 
-    def get_movie(self,query):
-        return AssertableResponse(self.get(query))
+    def get_movie(self,query,url):
+        return AssertableResponse(self.get(query,url))
 
 
--- Tests/test_api_omdb.py
+++ Tests/test_api_omdb.py
@@ -1,25 +1,26 @@
 from App.condition import status_code, body
 from App.core import CinemaApi
 from hamcrest import has_length,greater_than
+import os
 
 def test_search_movie_to_title():
     query = {'t':'Matrix'}
-    response = CinemaApi().get_movie(query=query)
+    response = CinemaApi().get_movie(query=query,url=os.environ["BASE_URL"])
     assert response.status_code(200)
     assert len(response.field('Title')) > 0
 
 def test_search_movie_to_id():
     query = {'i': 'tt1285016'}
-    response = CinemaApi().get_movie(query=query)
+    response = CinemaApi().get_movie(query=query,url=os.environ["BASE_URL"])
     assert response.status_code(200)
     assert len(response.field('Title')) > 0
 
 def test_search_movie_status_code():
     query = {'t': 'Matrix'}
-    response = CinemaApi().get_movie(query=query)
+    response = CinemaApi().get_movie(query=query,url=os.environ["BASE_URL"])
     response.should_have(status_code(200))
 
 def test_search_movie_field():
     query = {'t': 'Matrix'}
-    response = CinemaApi().get_movie(query=query)
+    response = CinemaApi().get_movie(query=query,url=os.environ["BASE_URL"])
     response.should_have(body("$.Title",has_length(greater_than(0))))
